cca5834d1122003c5c62136bac7bd273effd3600
Lluís Pérez Feliu <lluisperezfeliu@gmail.com>
2020-04-08 10:57:11 +0200

    Implementado LOG y constructor por defecto.

--- helloWorld/src/main/java/es/llpefe/demo/Manager.java
+++ helloWorld/src/main/java/es/llpefe/demo/Manager.java
@@ -1,10 +1,23 @@
 package es.llpefe.demo;
 
+import java.util.logging.Logger;
+
 /**
  * 
  * @author Lluís Pérez Feliu
  *
  */
 public class Manager {
-	
+
+	/**
+	 * Salida de log.
+	 */
+	public static final Logger LOG = Logger.getLogger(Manager.class.getName());
+
+	/**
+	 * Constructor por defecto.
+	 */
+	public Manager() {
+		super();
+	}
 }
